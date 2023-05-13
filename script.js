5c1942e0afcb0a3818ebe03d9c81d11957b0919c
JehanKandy <jehankandy@gmail.com>
2023-05-13 19:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var form = document.getElementById('myform');
 
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    var search = document.getElementById('search').value
+    alert(search)
+})
