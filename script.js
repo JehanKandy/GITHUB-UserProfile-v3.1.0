cb72c668f79ce331ea0139e141bc34db4b204172
JehanKandy <jehankandy@gmail.com>
2023-05-13 19:29:49 +0530

    Update

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ form.addEventListener('submit', function(e){
     e.preventDefault()
 
     var search = document.getElementById('search').value
-    alert(search)
+    
+    var oname = search.split(' ').join()
+    alert(oname);
+    
+    fetch("https://api.github.com/users/"+oname)
 })
