62cc05c9c4511957de839151a7419ab35ed407b8
JehanKandy <jehankandy@gmail.com>
2023-05-20 20:34:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -123,6 +123,7 @@ form.addEventListener('submit', function(e){
     .then((result_folllowing) => result_folllowing.json())
     .then((data_following) => {
 
+        var myorg = document.getElementById('');
 
     })
 })
