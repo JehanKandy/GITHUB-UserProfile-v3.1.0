16eb985723fc3adb7847808bdb06733270255f7f
JehanKandy <jehankandy@gmail.com>
2023-05-26 15:51:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ form.addEventListener('submit', function(e){
             var your_followers = "<img src='https://github.com/JehanKandy/GITHUB-UserProfile-v0.3.2/blob/master/images/bfollowers.png?raw=true' class='achie-img'>";
         }
         else if(data.followers >= 50 && data.followers < 100){
-            var your_followers = "<img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/bronze%20followers.png?raw=true' class='achie-img'><img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/siler%20followers.png?raw=true' class='achie-img'>";
+            var your_followers = "<img src='https://github.com/JehanKandy/GITHUB-UserProfile-v0.3.2/blob/master/images/bfollowers.png?raw=true' class='achie-img'><img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/siler%20followers.png?raw=true' class='achie-img'>";
         }
         else if(data.followers >= 100){
             var your_followers = "<img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/bronze%20followers.png?raw=true' class='achie-img'><img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/siler%20followers.png?raw=true' class='achie-img'><img src='https://github.com/JehanKandy/GITHUB-UserProfile/blob/master/images/gold%20followers.png?raw=true' class='achie-img'>";
