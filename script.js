6f6be3d81700fd9e23eb596d09c2086ed0742709
JehanKandy <jehankandy@gmail.com>
2023-06-01 07:26:12 +0530

    Update

--- script.js
+++ script.js
@@ -185,7 +185,8 @@ form.addEventListener('submit', function(e){
             for(let i = 0; i < data_myorg.length; i++){
                 //countorg += '<div>' + data_myorg[i].login + '</div>';
                 var org_url = "www.github.com/";
-                countorg += "<a href='" + org_url + "' target='_blank'><img src='" + data_myorg[i].avatar_url + "' class='follower-img'></a>";
+                //countorg += "<a href='" + org_url + "' target='_blank'><img src='" + data_myorg[i].avatar_url + "' class='follower-img'></a>";
+                countorg += "<a href='https://www.github.com'><img src='" + data_myorg[i].avatar_url + "' class='follower-img'></a>"
             }
         }
 
