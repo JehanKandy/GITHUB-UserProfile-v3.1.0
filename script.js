1140e93ffa04abf311a17697e97aed7ad3ae7ff8
JehanKandy <jehankandy@gmail.com>
2023-06-02 22:23:07 +0530

    Update

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ form.addEventListener('submit', function(e){
             let countrepos = "<hr><h2>My Repos</h2>";
             
             for(let j = 0; j < data_myrepos.length; j++){
-                countrepos += "My Repo" + data_myrepos[i].description;
+                countrepos += "My Repo" + data_myrepos[j].description
             }
     
             myrepos.innerHTML = countrepos;
