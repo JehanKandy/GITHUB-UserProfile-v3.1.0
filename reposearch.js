edac5923f903a8943a9ce70c558b31e593835d3f
JehanKandy <jehankandy@gmail.com>
2023-06-06 08:38:45 +0530

    Update

--- reposearch.js
+++ reposearch.js
@@ -3,6 +3,6 @@ var repoform = document.getElementById('reposearch');
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
-    var git_user = document.forms["reposearch"]["search"].value;
+    var git_user = document.forms["reposearch"]["repos_search"].value;
     var git_repo = document.forms["reposearch"][].value;
 })
