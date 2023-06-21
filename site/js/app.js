74f69ebe14cb41effbc70dfbf9998eb3bff42c62
JehanKandy <jehankandy@gmail.com>
2023-06-21 08:59:54 +0530

    Update

--- site/js/app.js
+++ site/js/app.js
@@ -405,7 +405,7 @@ form.addEventListener('submit', function(e){
             repos_all += `<div class='col-lg-6'>`
             
             for(let i = 0; i < repo_stargazers_data.length; i++){
-                repos_all += `<h4>${repo_stargazers_data[i].login}</h4>`;
+                repos_all += `<span><h4>${repo_stargazers_data[i].login}</h4> HI</span>`;
             }
 
             repos_all += `</div>
